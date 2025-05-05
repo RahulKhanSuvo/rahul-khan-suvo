c4216802ad233a4bf6ff4d7db724cd04a1e34a44
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-05-05 17:08:47 +0600

    Add Hero component and integrate it into Sections; establish layout structure for the main content

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <section className="relative flex flex-row items-center z-20 mx-auto">
+      <div className="flex flex-col gap-[13px] items-center">
+        <h1 className="text-2xl/1"></h1>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Sections.tsx
@@ -0,0 +1,9 @@
+import Hero from "./Hero/Hero";
+
+export default function Sections() {
+  return (
+    <main className="flex flex-col gap-[142px] w-fit md:max-w-screen-lg pt-[236px] md:pt-60 mx-auto">
+      <Hero />
+    </main>
+  );
+}
