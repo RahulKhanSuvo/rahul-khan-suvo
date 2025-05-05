56c4c03da20c468095f7af38940de2d0c1dfa615
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-05-05 14:46:26 +0600

    Refactor TopBackground component to include multiple icons with improved layout and accessibility; update styles for circle icons in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     <div className="relative overflow-clip" id="home">
+      <div className="w-screen h-screen"></div>
       <NavBar />
       <TopBackground />
     </div>
--- src/components/Hero/TopBackground.tsx
+++ src/components/Hero/TopBackground.tsx
@@ -2,17 +2,68 @@ import Image from "next/image";
 
 export default function TopBackground() {
   return (
-    <>
-      <div className="absolute w-full max-w-[360px] md:max-w-screen h-[285px] md:h-[656px] top-[168px] md:top-11 left-1/2 -translate-1/2 md:overflow-x-hidden z-10">
-        <div className="w-full h-full relative circle-icon top-0 left-[171px] md:top-28 md:left-72">
+    <div className="absolute w-full max-w-[360px] md:max-w-screen h-[285px] md:h-[656px] top-[188px] md:top-11 left-1/2 -translate-x-1/2 md:overflow-x-hidden z-10">
+      <div className="w-full h-full relative">
+        {/* Rocket icon - top center */}
+        <div className="circle-icon absolute top-0 left-[171px] md:top-20 md:left-72">
           <Image
             width={23}
-            height={23}
+            alt="Rocket icon"
             src={"/rocket_icon.svg"}
-            alt="rocket icon"
+            height={23}
+          />
+        </div>
+
+        {/* Bracket icon - bottom left */}
+        <div className="circle-icon absolute top-[162px] left-[71px] md:top-64 md:left-0">
+          <Image
+            width={23}
+            alt="Bracket icon"
+            src={"/bracket_icon.svg"}
+            height={23}
+          />
+        </div>
+
+        {/* GitHub icon - center right */}
+        <div className="circle-icon absolute top-60 left-[200px] md:top-80 md:left-[80%]">
+          <Image
+            width={23}
+            alt="GitHub icon"
+            src={"/github_icon.svg"}
+            height={23}
+          />
+        </div>
+
+        {/* Electricity icon - center left */}
+        <div className="circle-icon absolute top-60 left-[50px] md:top-80 md:left-[20%]">
+          <Image
+            width={23}
+            alt="Electricity icon"
+            src={"/electricity_icon.svg"}
+            height={23}
+          />
+        </div>
+
+        {/* Merge icon - top right */}
+        <div className="circle-icon absolute top-20 left-[250px] md:top-40 md:left-[85%]">
+          <Image
+            width={23}
+            alt="Merge icon"
+            src={"/merge_icon.svg"}
+            height={23}
+          />
+        </div>
+
+        {/* Stack icon - bottom right */}
+        <div className=" hidden md:flex circle-icon absolute top-[200px] left-[250px] md:top-[70%] md:left-[85%]">
+          <Image
+            width={23}
+            alt="Stack icon"
+            src={"/stack_icon.svg"}
+            height={23}
           />
         </div>
       </div>
-    </>
+    </div>
   );
 }
