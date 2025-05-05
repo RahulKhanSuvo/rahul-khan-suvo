38ec2289aa4f04464a2009a7d35ffa082bb0ecb9
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-05-05 12:46:57 +0600

    Implement toggle functionality for NavBar and enhance section highlighting

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
   const [activeSection, setActiveSection] = useState("home");
   useEffect(() => {
     setActiveSection("home");
@@ -16,7 +17,10 @@ function NavBar() {
   };
   return (
     <div className="fixed top-12 right-6 mx-auto flex flex-col gap-2.5 items-end z-50">
-      <button className="bg-background card-shadow p-3 rounded">
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="bg-background card-shadow p-3 rounded"
+      >
         <Image
           alt="light"
           src="/menu_icon_light.svg"
@@ -32,19 +36,41 @@ function NavBar() {
           className="dark:block hidden"
         />
       </button>
-      <nav>
+      <nav
+        className={classNames(
+          "bg-background card-shadow p-3 duration-300 ease-in-out rounded md:block",
+          { "opacity-100": isOpen, "opacity-0": !isOpen }
+        )}
+      >
         <ul>
           <li>
             <div
               onClick={() => {
                 scrollToSection("home");
                 setActiveSection("home");
+                setIsOpen(false);
               }}
               className={classNames("rounded p-1", {
-                "bg-[var(--primary)] text-white": true,
+                "bg-[var(--primary)] text-white": activeSection === "home",
+                "text-[var(--primary)]": activeSection !== "home",
               })}
             >
               Home
+            </div>
+          </li>
+          <li>
+            <div
+              onClick={() => {
+                scrollToSection("skills");
+                setActiveSection("skills");
+                setIsOpen(false);
+              }}
+              className={classNames("rounded p-1", {
+                "bg-[var(--primary)] text-white": activeSection === "skills",
+                "text-[var(--primary)]": activeSection !== "skills",
+              })}
+            >
+              Skills
             </div>
           </li>
         </ul>
