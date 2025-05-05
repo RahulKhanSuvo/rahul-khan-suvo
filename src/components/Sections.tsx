8e63e494d90e3d44111647e1a0004d99a95d667f
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-05-05 21:07:39 +0600

    Add Projects component and integrate it into Sections; initialize projects data

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -1,12 +1,14 @@
 import Hero from "./Hero/Hero";
+import Projects from "./Projects/Projects";
 import Skills from "./Skills/Skills";
 import skills from "@/data/skills.json";
 
 export default function Sections() {
   return (
-    <main className="flex flex-col  min-h-screen  gap-[142px] w-full md:max-w-screen-lg  md:pt-32 mx-auto">
+    <main className="flex flex-col  min-h-screen  gap-[100px] w-full md:max-w-screen-lg  md:pt-32 mx-auto">
       <Hero />
       <Skills />
+      <Projects />
     </main>
   );
 }
