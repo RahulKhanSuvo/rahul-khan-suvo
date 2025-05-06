b8b99fa782cda245ba65907bd3bd56e6cab643e2
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-05-06 14:05:09 +0600

    Add Skill component: implement mouse tracking effect with radial gradient mask

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -1,8 +1,35 @@
+"use client";
 import Image from "next/image";
-
+import { motion, useMotionTemplate, useMotionValue } from "motion/react";
+import { useEffect, useRef } from "react";
 export default function Skill({ name, icon }: { name: string; icon: string }) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const xDistance = useMotionValue(0);
+  const yDistance = useMotionValue(0);
+  const mask = useMotionTemplate`radial-gradient(100px 100px at ${xDistance}px ${yDistance}px,#000,transparent) `;
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!ref.current) return;
+    const clientRect = ref.current.getBoundingClientRect();
+    xDistance.set(e.x - clientRect.x);
+    yDistance.set(e.y - clientRect.y);
+  };
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
   return (
     <div className="relative flex gap-2 p-2 border-[var(--primary)] border rounded-lg h-[46px]">
+      <motion.div
+        ref={ref}
+        className="absolute inset-0 border-2 border-purple-500 dark:border-purple-300 rounded-lg"
+        style={{
+          maskImage: mask,
+          WebkitMaskImage: mask,
+        }}
+      ></motion.div>
       <Image width={20} height={20} src={icon} alt={`${name} icon`} />
       <p className="text-lg">{name}</p>
     </div>
