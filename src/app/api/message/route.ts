a40e1e25d10e314573847e97efbf7c07559c016c
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-05-09 06:55:01 +0600

    Enhance message API route to include email sending functionality and error handling

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,7 +1,35 @@
-import { NextRequest } from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 export const POST = async (req: NextRequest) => {
-    const body = await req.json()
-    const { name, email, message } = body
-    console.log(body);
+    try {
+        const body = await req.json()
+        const { name, email, message } = body
+        if (!name || !email || !message) {
+            return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+        }
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+        const mailOptions = {
+            from: `"${name}" < ${email}>`,
+            to: process.env.EMAIL_USER,
+            subject: "New Contact Form Submission",
+            text: message,
+            html: `<p><strong>Name:</strong> ${name}</p>
+             <p><strong>Email:</strong> ${email}</p>
+             <p><strong>Message:</strong><br/>${message}</p>`,
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Message sent: %s", info.messageId);
+
+        return NextResponse.json({ success: true });
+
+    } catch (error) {
+
+    }
 }
