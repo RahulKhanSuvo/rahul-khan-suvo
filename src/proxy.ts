e507ec8988a9a28cc7c5a880be77abd77b068bd2
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-25 15:37:51 +0600

    feat: implement admin dashboard, login page, and authentication proxy while removing test database script

---
+++ src/proxy.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export async function proxy(request: NextRequest) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+    console.log(session);
+
+    if (!session && request.nextUrl.pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*"],
+};
