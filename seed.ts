c057d30c7ce681013c8f28f52a09ab0d1405a6b5
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-25 15:28:45 +0600

    feat: integrate better-auth for authentication and restructure app directory layout

---
+++ seed.ts
@@ -0,0 +1,19 @@
+import { auth } from "./src/lib/auth";
+
+async function createAdmin() {
+    console.log("Creating admin user...");
+
+    // Better Auth has an internal API we can call directly
+    await auth.api.signUpEmail({
+        body: {
+            email: "admin@gmail.com",
+            password: "123456",
+            name: "Md. Rahul Khan Suvo",
+        }
+    });
+
+    console.log("✅ Admin user created!");
+    process.exit(0);
+}
+
+createAdmin();
---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.DATABASE_URL!);
+const db = client.db();
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db),
+    emailAndPassword: {
+        enabled: true
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+    },
+});
