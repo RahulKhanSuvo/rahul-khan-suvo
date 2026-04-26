3c0c149cbaf06624d4389eb2344483c6db7e0132
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-26 18:25:11 +0600

    fix: add suppressHydrationWarning to html and body tags in root layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased `}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased `} suppressHydrationWarning>
         <ScrollProgressBar />
         <Toaster
           position="top-center"
