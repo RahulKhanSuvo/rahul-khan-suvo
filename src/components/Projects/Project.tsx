b3f866502c2775e7951ad1bc4ceb49fdc4fbeb2b
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-26 21:56:13 +0600

    style: update project link button background to primary color

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -170,7 +170,7 @@ export default function Project({
               </div>
               <div>
                 <Link
-                  className="flex items-center gap-2 px-4 py-2 rounded-lg bg-white/5 hover:bg-white/10 transition-colors border dark:border-white/5 border-gray-500 backdrop-blur-md"
+                  className="flex items-center gap-2 px-4 py-2 rounded-lg bg-primary hover:bg-primary/80 transition-colors border dark:border-white/5 border-gray-500 backdrop-blur-md"
                   href={`/project/${id}`}
                 >
                   <FaArrowUpRightFromSquare />
